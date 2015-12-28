f7668142b66606b13a9f84d677cc214cdaf4c2fd
syedfaizan <sfaizan@addomacto.com>
2015-12-28 15:52:04 +0530

    Improvements
    - categorized UV values into ranges and named them low, moderate, high,
    very high and extreme
    - the table is now colored based on the UV ranges,
    green,yellow,orange,red,blue respectively
    - on hover on the table, shows a small tooltip showing the exposure
    levels.

--- api/UVIndex.js
+++ api/UVIndex.js
@@ -14,6 +14,24 @@ router.post("/",function(req,res){
 	};
 	if(city && state){
 	  controllerObj.getUVIndexes(location,retval,function(val){
+		// group the UV indexes into categories
+		val.map(function(result){
+			if(result){
+				var UV_VALUE = result.UV_VALUE;
+				if(UV_VALUE < 3)
+					result['EXPOSURE'] = "Low";
+				if(UV_VALUE >= 3 && UV_VALUE <= 5)
+					result['EXPOSURE'] = "Moderate";
+				if(UV_VALUE >= 6 && UV_VALUE <=7)
+					result['EXPOSURE'] = "High";
+				if(UV_VALUE >=8 && UV_VALUE <=10)
+					result['EXPOSURE'] = "VeryHigh";
+				if(UV_VALUE >= 11)
+					result['EXPOSURE'] = "Extreme";
+				// return Modified array element
+				return result;
+			}
+		});
 		retval.data = val;
 		res.send(retval);
 	   });
