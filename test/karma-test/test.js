809c07bd812d354d07c7ab29ba61203252b743d3
Chetan <cprakash@comtechllc.com>
2015-12-30 15:22:46 +0530

    HOUND CI TEST

--- test/karma-test/test.js
+++ test/karma-test/test.js
@@ -1,9 +1,5 @@
-var expect  = chai.expect;
 describe('Home Controllers', function(){
 	beforeEach(module('EPA_RFI'));
-	describe('homeCtrl',function(){
-		
-	});
 	var scope, ctrl, $httpBackend,httpHelper;
 	beforeEach(inject(function(_$httpBackend_,_httpHelper_, $rootScope, $controller) {
 		$httpBackend = _$httpBackend_;
@@ -11,12 +7,11 @@ describe('Home Controllers', function(){
 		scope ={};
 		ctrl = $controller('homeCtrl', {$scope: scope});
 	}));
-	it('should clear error boxes',inject(function($controller){
-		var homeController = ctrl
+	it('should clear error boxes',function(){
 		scope.clearErrorBoxes();
 		expect(scope.noResponseError).to.be.false;
 		expect(scope.invalidInputError).to.be.false;
-	}));
+	});
 	it('service should get uvindexes',function(){
 		var testResut = {"success":true,"data":[
 			{"SEQUENCE":1,"CITY":"ATLANTA","STATE":"GA",
@@ -25,8 +20,8 @@ describe('Home Controllers', function(){
   			.respond(testResut);
 		 httpHelper.queryByLocation("ATLANTA","GA").then(function(retval) {
 			expect(retval.data.success).to.be.true;
-			expect(retval.data.data["SEQUENCE"]).to.equal(testResut["data"]["SEQUENCE"]);
-			expect(retval.data.data["CITY"]).to.equal(testResut["data"]["CITY"]);
+			expect(retval.data.data.SEQUENCE).to.equal(testResut.data.SEQUENCE);
+			expect(retval.data.data.CITY).to.equal(testResut.data.CITY);
     		});
 		$httpBackend.flush();	
 	});
