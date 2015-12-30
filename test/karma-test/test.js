84651202d4853cb13c36ba665d7edef0db18c1eb
Chetan <cprakash@comtechllc.com>
2015-12-30 15:50:54 +0530

    Fix issue

--- test/karma-test/test.js
+++ test/karma-test/test.js
@@ -1,22 +1,24 @@
-describe('Home Controllers', function(){
-	beforeEach(module('EPA_RFI'));
+describe("Home Controllers", function(){
+	beforeEach(module("EPA_RFI"));
 	var scope, ctrl, $httpBackend,httpHelper;
-	beforeEach(inject(function(_$httpBackend_,_httpHelper_, $rootScope, $controller) {
+	beforeEach(inject(function(_$httpBackend_,_httpHelper_
+		, $rootScope, $controller) {
 		$httpBackend = _$httpBackend_;
 		httpHelper   = _httpHelper_;
 		scope ={};
-		ctrl = $controller('homeCtrl', {$scope: scope});
+		ctrl = $controller("homeCtrl", {$scope: scope});
 	}));
-	it('should clear error boxes',function(){
+	it("should clear error boxes",function(){
 		scope.clearErrorBoxes();
 		expect(scope.noResponseError).to.be.false;
 		expect(scope.invalidInputError).to.be.false;
 	});
-	it('service should get uvindexes',function(){
+	it("service should get uvindexes",function(){
 		var testResut = {"success":true,"data":[
 			{"SEQUENCE":1,"CITY":"ATLANTA","STATE":"GA",
-			"DATE_TIME":"DEC/29/2015 06 AM","UV_VALUE":0,"EXPOSURE":"Low"}]};
-		 $httpBackend.whenPOST('/api/getUVIndex')
+			"DATE_TIME":"DEC/29/2015 06 AM","UV_VALUE":0,
+			"EXPOSURE":"Low"}]};
+		 $httpBackend.whenPOST("/api/getUVIndex")
   			.respond(testResut);
 		 httpHelper.queryByLocation("ATLANTA","GA").then(function(retval) {
 			expect(retval.data.success).to.be.true;
